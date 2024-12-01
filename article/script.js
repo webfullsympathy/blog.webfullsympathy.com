898ad947cc20dee397017014fb3715ccf56871df
ウェブ完理 <webfullsympathy@ymail.ne.jp>
2024-12-01 10:40:33 +0900

    無理やり直したの巻

--- article/script.js
+++ article/script.js
@@ -21,6 +21,15 @@ if(pageNumber === null){
 
 function doc(){
     try{
+    // 記事詳細
+    fetch("https://raw.githubusercontent.com/webfullsympathy/blog.webfullsympathy.com/refs/heads/doc/" + pageNumber + "/set.txt")
+    .then(response => response.text())
+    .then(data => {
+        const data_list = data.split(/\n/)
+
+        document.title = data_list[0] + " | ウェブ完理ブログ"
+    })
+
     // 記事内容
     fetch("https://raw.githubusercontent.com/webfullsympathy/blog.webfullsympathy.com/refs/heads/doc/" + pageNumber + "/main.md")
     .then(response => response.text())
@@ -28,21 +37,14 @@ function doc(){
         if(data === "404: Not Found"){
             document.getElementById("main").innerHTML = "<h1>お探しのページが見つかりませんでした。</h1><br><a href='https://blog.webfullsympathy.com'><p>ホームに戻る</p></a>"
 
-            document.getElementById("main").style.display = "block"
             document.getElementById("main").style.margin = "0 auto"
             document.getElementById("main").style.border = "1px #eee solid"
             document.getElementById("main").style.borderRadius = "30px"
             document.getElementById("main").style.background = "#fff"
             document.getElementById("main").style.width = "80%"
         }else{
-            document.getElementById("main").innerHTML = marked.parse(data) + "<br><a href='https://share-tool.net?text=" + document.title + " - " + location.href + "' target='_blank'><img src='https://share-tool.net/src/share.jpg' style='border-radius: 100%;width: 200px'></a>"
-
-            document.getElementById("main").style.display = "block"
-            document.getElementById("main").style.margin = "0 auto"
-            document.getElementById("main").style.border = "1px #eee solid"
-            document.getElementById("main").style.borderRadius = "30px"
-            document.getElementById("main").style.background = "#fff"
-            document.getElementById("main").style.width = "80%"
+            document.getElementById("main").innerHTML = marked.parse(data) + 
+            "<br><a href='https://share-tool.net?text=" + document.title + " - " + location.href + "' target='_blank'><img src='https://share-tool.net/src/share.jpg' style='border-radius: 100%;width: 200px'></a>"
         }
     })
     }
